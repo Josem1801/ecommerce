8f5df70176e537b34833b53079a58817b816f73f
Jose Manuel Rosales <josemanuelrosales2@gmail.com>
2022-05-03 14:04:59 -0500

    Inject the server side rendered styles

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,58 @@
-import {
-  Html, Head, Main, NextScript,
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Html,
+  Main,
+  NextScript,
+  Head,
 } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <style data-href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@200;300;400;500;600;700&display=swap" />
-        <style data-href="https://fonts.googleapis.com/css2?family=Lato&display=swap" />
-        <style data-href=" https://fonts.googleapis.com/css2?family=Water+Brush&display=swap" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    // eslint-disable-next-line testing-library/render-result-naming-convention
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        // eslint-disable-next-line implicit-arrow-linebreak
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            // eslint-disable-next-line implicit-arrow-linebreak
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>,
+        ],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <style data-href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@200;300;400;500;600;700&display=swap" />
+          <style data-href="https://fonts.googleapis.com/css2?family=Lato&display=swap" />
+          <style data-href=" https://fonts.googleapis.com/css2?family=Water+Brush&display=swap" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
