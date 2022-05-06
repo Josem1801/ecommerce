8e6a6f8fbb777949dd4efc624dc854dfdeb450b3
Jose Manuel Rosales <josemanuelrosales2@gmail.com>
2022-05-06 14:02:23 -0500

    typography theme setup & overflow fixed

---
+++ src/theme/typography.ts
@@ -0,0 +1,53 @@
+const typography = {
+  header: {
+    heading1: {
+      fontSize: 48,
+      fontWeight: 600,
+      lineHeight: 56,
+    },
+    heading2: {
+      fontSize: 40,
+      fontWeight: 600,
+      lineHeight: 48,
+    },
+    heading3: {
+      fontSize: 32,
+      fontWeight: 600,
+      lineHeight: 40,
+    },
+    display1: {
+      fontSize: 66,
+      fontWeight: 600,
+      lineHeight: 76,
+    },
+  },
+  text: {
+    xlBody600: {
+      fontSize: 28,
+      fontWeight: 600,
+      lineHeight: 28,
+    },
+    xlBody400: {
+      fontSize: 20,
+      fontWeight: 400,
+      lineHeight: 28,
+    },
+    bodyLarge: {
+      fontSize: 20,
+      fontWeight: 600,
+      lineHeight: 28,
+    },
+    bodyRegular: {
+      fontSize: 16,
+      fontWeight: 400,
+      lineHeight: 24,
+    },
+    bodySmall: {
+      fontSize: 14,
+      fontWeight: 500,
+      lineHeight: 18,
+    },
+  },
+};
+
+export default typography;
