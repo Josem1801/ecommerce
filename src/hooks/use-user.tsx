fb2aa7f1323d9fe49088957354e2636b55b6723e
Jose Manuel Rosales <josemanuelrosales2@gmail.com>
2022-05-16 16:08:54 -0500

    fix error ts

--- src/hooks/use-user.tsx
+++ src/hooks/use-user.tsx
@@ -2,10 +2,7 @@ import { getUserData } from 'services/auth';
 import useSWR from 'swr';
 
 export default function useUser() {
-  const { data, mutate, error } = useSWR(
-    '/login/user',
-    () => getUserData()?.user,
-  );
+  const { data, mutate, error } = useSWR('/login/user', () => getUserData());
   const user = data || null;
   const loading = !error && !data;
   const setUser = mutate;
